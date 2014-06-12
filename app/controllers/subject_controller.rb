d5608c66eefe4fa3f6e280cd1c80a53b054783c6
Alfonso Mancilla ALvis <almancill@gmail.com>
2014-06-11 19:50:33 -0500

    update for 201430 period

--- app/controllers/subject_controller.rb
+++ app/controllers/subject_controller.rb
@@ -16,7 +16,7 @@ class SubjectController < ApplicationController
 		info_regex = /Materia: (?<mat>[A-Z]{3}[0-9]{4} - [0-9]{1,2})NRC: (?<nrc>[0-9]{4}).+Matriculados: (?<used>[0-9]+)Cupos Disponibles: (?<available>[0-9]+)/	
 		pattern = /([a-zA-Z]{3})(\d{4})/
 		pattern =~ @subject_code
-		response = Typhoeus::post("http://guayacan.uninorte.edu.co/registro/resultado_curso.asp", body: {valida: "OK", mat2: $1, curso: $2, BtnCurso: "Buscar", datos_periodo: "201410", nom_periodo: "Primer Semestre 2014"})
+		response = Typhoeus::post("http://guayacan.uninorte.edu.co/registro/resultado_curso.asp", body: {valida: "OK", mat2: $1, curso: $2, BtnCurso: "Buscar", datos_periodo: "201430", nom_periodo: "Segundo Semestre 2014"})
 		doc = Nokogiri::HTML(response.body)
 		tables = doc.css("table[cellpadding='0'][align='center']")
 
