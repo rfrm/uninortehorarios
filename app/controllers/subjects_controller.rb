1649f68e6c13a61b398dbda45539176ac2fe282b
Robinson Rodriguez <rfrodriguez1992@gmail.com>
2016-06-25 23:19:20 -0500

    Load DB pool size fron env variable. Add a time to allow for cache update

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -4,7 +4,8 @@ class SubjectsController < ApplicationController
     @subject = Subject.find_by code: subject_code
     courses = @subject.parsed_data
     subject_teachers = courses.map{ |h| h["lecture_teachers"] }.flatten.uniq.sort
-    expires_in @subject.expires_in, public: true
+
+    expires_in @subject.expires_in, public: true, race_condition_ttl: 2.minutes.to_i
     render json: {name: courses[0]["name"], mat: subject_code,
                   subject_teachers: subject_teachers, courses: courses }
   end
