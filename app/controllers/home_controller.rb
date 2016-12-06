1ada6478416aea56463092acd876a44d00b2f8ac
Alfonso Mancilla <almancill@gmail.com>
2016-12-06 23:41:11 +0100

    Enable browser cache for HomeController#index using the ETAG header

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,11 @@
 class HomeController < ApplicationController
+
+  def index
+    #
+    # Modify the :etag value everytime a change is made to the
+    # view rendered by this action to tell the browser
+    # to update their cached version of the page.
+    #
+    fresh_when etag: '8e5db4379e43f5655185065849574244', public: true
+  end
 end
