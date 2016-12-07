ab0a8ba60a1c860d68629ef32d8db2ec46296a5a
Robinson Rodriguez <rfrodriguez1992@gmail.com>
2016-12-06 20:01:06 -0500

    Cache / and /generate pages

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,9 @@
 class HomeController < ApplicationController
+  def index
+    fresh_when etag: '32747fdf7cc0dc321ea967cbe89e3b04', public: true
+  end
+
+  def generate
+    fresh_when etag: '8e5db4379e43f5655185065849574244', public: true
+  end
 end
