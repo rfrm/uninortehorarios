b215d7a52afebfd3f77780837414fa61c273de55
Robinson Rodriguez <rfrodriguez1992@gmail.com>
2016-12-14 16:37:06 -0500

    Drop cached page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,9 @@
 class HomeController < ApplicationController
   def index
-    fresh_when etag: '32747fdf7cc0dc321ea967cbe89e3b04', public: true
+    fresh_when etag: '32747fdf7cc0dc321ea967cbe89e3b05', public: true
   end
 
   def generate
-    fresh_when etag: '8e5db4379e43f5655185065849574245', public: true
+    fresh_when etag: '8e5db4379e43f5655185065849574246', public: true
   end
 end
