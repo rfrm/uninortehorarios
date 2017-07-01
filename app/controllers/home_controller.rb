6cdb5c06a0253ece2d6f8072b8574546bdb33c32
Robinson Rodriguez <rfrodriguez1992@gmail.com>
2017-07-01 14:25:56 -0500

    Update message

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,9 @@
 class HomeController < ApplicationController
   def index
-    fresh_when etag: '32747fdf7cc0dc321ea967cbe89e3b06', public: true
+    fresh_when etag: '32747fdf7cc0dc321ea967cbe89e3b07', public: true
   end
 
   def generate
-    fresh_when etag: '8e5db4379e43f5655185065849574247', public: true
+    fresh_when etag: '8e5db4379e43f5655185065849574248', public: true
   end
 end
