d039c17b6cfeab1093f5fe26705c9eff6ce07773
Robinson Rodriguez <rfrodriguez1992@gmail.com>
2017-07-11 23:17:22 -0500

    Update autocomplete data

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,9 @@
 class HomeController < ApplicationController
   def index
-    fresh_when etag: '32747fdf7cc0dc321ea967cbe89e3b10', public: true
+    fresh_when etag: '32747fdf7cc0dc321ea967cbe89e3b11', public: true
   end
 
   def generate
-    fresh_when etag: '8e5db4379e43f5655185065849574251', public: true
+    fresh_when etag: '8e5db4379e43f5655185065849574252', public: true
   end
 end
